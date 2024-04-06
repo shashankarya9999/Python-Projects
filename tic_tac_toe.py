4cbb3b7092326e99baa1e11bd3e10e92d8efda37
Shashank Arya <shashankarya9831@gmail.com>
2024-04-06 14:37:10 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,16 +1,14 @@
 import random
 
-print('Welcome to the game of Tic Tac Toe\n')
-
 # function that prints out a 3x3 board
 def display_board(board):
-
     print(board[1]+'|'+board[2]+'|'+board[3])
     print(board[4]+'|'+board[5]+'|'+board[6])
     print(board[7]+'|'+board[8]+'|'+board[9])
 
 board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
+print('Welcome to the game of Tic Tac Toe\n')
 print("The following is the indexing method. Input accordingly!\n")
 
 placeholder_board = ['#','1','2','3','4','5','6','7','8','9']
@@ -20,7 +18,6 @@ print('\n')
 
 # function that can take in a player input and assign their marker as 'X' or 'O'
 def player_input():
-
     marker = ''
 
     while marker != 'X' and marker != 'O':
@@ -38,12 +35,10 @@ def player_input():
 
 # function that takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board
 def place_marker(board,marker,position):
-
     board[position] = marker
 
 # function that takes in a board list object and checks if someone has won
 def win_check(board,mark):
-
     return((board[1] == mark and board[2] == mark and board[3] == mark ) or     #row
     (board[4] == mark and board[5] == mark and board[6] == mark ) or            #row
     (board[7] == mark and board[8] == mark and board[9] == mark ) or            #row
@@ -64,7 +59,6 @@ def choose_first():
 
 # function that returns a boolean indicating whether a space on the board is freely available
 def space_check(test_board,position):
-
     return test_board[position] == ' '
 
 # function that checks if the board is full and returns a boolean value. True if full, False otherwise
@@ -77,7 +71,6 @@ def full_board_check(test_board):
 # function that asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if its a free position.
 # if it is, then return the position for later use
 def player_choice(test_board):
-
     position = 0
 
     while position not in [1,2,3,4,5,6,7,8,9] or not space_check(test_board,position):
@@ -87,13 +80,11 @@ def player_choice(test_board):
 
 # function that asks the player if they want to play again and returns a boolean True if they do want to play again
 def replay():
-
     choice = input("Play Again? Enter Yes or No: ").lower()
     return choice == 'yes'
 
 # function that maintains a leaderboard
-def leaderboard(player1_count,player2_count):
-    
+def leaderboard(player1_count,player2_count):    
     print('Current Leaderboard!')
     print("Player1's score: {}".format(player1_count))
     print("Player2's score: {}".format(player2_count))
@@ -104,7 +95,6 @@ player1_count = 0
 player2_count = 0
 
 while True:
-
     the_board = [' ']*10
     player1_marker , player2_marker = player_input()
 
@@ -119,9 +109,7 @@ while True:
         game_on = False
 
     while game_on:
-
         if turn == 'Player 1':
-
             # show the board    
             display_board(the_board)
             # choose a position
@@ -144,7 +132,6 @@ while True:
                     turn = 'Player 2'
 
         else:
-
             # show the board    
             display_board(the_board)
             # choose a position
